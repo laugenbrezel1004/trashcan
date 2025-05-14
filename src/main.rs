ec65e18ab24eaac83f14796c2fe07114908e8cc9
laurenz <laurenzschmidt214@gmail.com>
2025-05-14 18:18:50 +0200

    make trashdir

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ mod permissions; // Deklariert das Modul permissions (entspricht permissions.rs)
 use std::env;
 use std::fs;
 use std::path::Path;
+use nix::libc::ftok;
+use nix::unistd;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     #[cfg(debug_assertions)]
     println!("args => {:?}", args);
     let programmname = args[0].clone();
-
+    check_trashcan();
     // Überspringe den ersten Argument (Programmname)
     for arg in args.iter().skip(1) {
         check_existence(&arg, &programmname);
@@ -19,8 +21,10 @@ fn main() {
 
 // Prüft, ob die Datei existiert
 fn check_existence(argument: &str, programmname: &str)  {
-    if Path::new(argument).exists() {
-            println!("removing {}", argument);
+    //if Path::new(argument).exists() {
+    if fs::exists(argument).expect("Something went horribly wrong") {
+        #[cfg(debug_assertions)]
+        println!("removing {}", argument);
         fs::remove_file(argument).unwrap();
     }
     else {
@@ -31,7 +35,19 @@ fn check_existence(argument: &str, programmname: &str)  {
 
 fn delete_file(argument: &str) -> Result<(), String> {
     fs::remove_file(argument).map_err(|e| e.to_string())?;
+    fs::
     Ok(())
+}
+
+fn check_trashcan() {
+    let uid = unistd::getuid();
+    let dirname = format!("/tmp/trashcan-{}", uid);
+    if !fs::exists(&dirname).expect("Something went horribly wrong") {
+        //evtl noch den Fehler behandeln hahah
+        #[cfg(debug_assertions)]
+        println!("make trashcan");
+        fs::create_dir(dirname).unwrap();
+    }
 }
 
 
