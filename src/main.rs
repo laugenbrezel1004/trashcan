082fd4d4f20ec767247b6da3f44fce018e6a02de
laurenz <laurenzschmidt214@gmail.com>
2025-05-16 19:21:31 +0200

    merge flags.rs into main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,77 @@ use users::os::unix::UserExt;
 use trashcan::Trashcan;
 
 fn main() {
+
+
+    use std::cmp::min;
+    use clap::{Arg, Command};
+    use std::env;
+    use nix::libc::exit;
+    use crate::file;
+
+    pub fn check_flags() {
+
+        let args: Vec<String> = env::args().collect();
+        let matches = Command::new("trashcan")
+            .version("0.1.0")
+            .author("Laurenz Schmidt")
+            .about("rm replacement for dummies")
+            .override_usage("trashcan [FLAGS] FileToDelete")
+            .after_help(
+                "ENVIRONMENT VARIABLES:\n\
+Nothing",
+            )
+            .arg(
+                Arg::new("force")
+                    .long("force")
+                    .value_name("FILE")
+                    .help("destroy file forever and ever - without idiot protection")
+                    //.action(clap::ArgAction::SetTrue)
+                    .num_args(1..),
+            )
+            .arg(
+                Arg::new("show_config")
+                    .short('s')
+                    .long("show-config")
+                    .help("Show the current config file")
+                    .action(clap::ArgAction::SetTrue),
+            )
+            .arg(
+                Arg::new("files")
+                    .help("Files to delete")
+                    .num_args(1..) // Mindestens ein positionales Argument
+                    .index(1), // Positionale Argumente ab Index 2
+            )
+            .get_matches_from(env::args().collect::<Vec<String>>());
+
+        if matches.contains_id("force") {
+            println!("Forced flag present, will be overwritten");
+            // Zugriff auf alle Werte von --force
+            if let Some(force_args) = matches.get_many::<String>("force") {
+                #[cfg(debug_assertions)]
+                println!("Force arguments:");
+                for arg in force_args {
+                    println!("  - {}", arg);
+                }
+            } else {
+                println!("trashcan: missing operand\nTry \'trashcan --help\' for more information.");
+            }
+        }
+        if let Some(files) = matches.get_many::<String>("files") {
+            #[cfg(debug_assertions)]
+            println!("Files to delete:");
+            file::check_existence(&arg, trashcan1.location);
+            //damit "trashcan" nicht als file gewertet wird
+            let new_files = files.skip(1);
+            for file in new_files {
+                #[cfg(debug_assertions)]
+                println!("  - {}", file);
+            }
+        } else {
+            eprintln!("No files specified");
+        }
+    }
+
     let args: Vec<String> = env::args().collect();
 
     #[cfg(debug_assertions)]
@@ -36,9 +107,9 @@ fn main() {
     // build trashcan if not available
     trashcan1.check_trashcan();
 
-    for arg in args.iter().skip(1) {
+    //skip programname
+    flags::check_flags();
+    //for arg in args.iter().skip(1) {
         //check if files exists and delete if it does
-       // flags::check_flags();
-        file::check_existence(&arg, trashcan1.location);
-    }
+    //}
 }
